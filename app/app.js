e23ea9b398016f59bda36e8ba6d2ccbe3f42cbca
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-07-28 13:43:26 +0600

    add Reate Limit

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const {
   globalErrorHandler,
   catchAllUndefinedRoutes,
@@ -16,6 +17,17 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+// Rate limiting middleware with options
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+});
+
+// Apply the rate limiter to all requests
+app.use(limiter);
+
 app.use(express.json());
 
 // Define routes
